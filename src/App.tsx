624b9eca753bead0bc67240b088e30181f7e4353
Habiba22Akter <habiba22akter39@gmail.com>
2026-09-11 20:15:36 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+ 
+
+  return (
+    <>
+      <h2 className="text-2xl font-bold text-cyan-500">Welcome to the App</h2>
+      <button className="btn btn-neutral">Neutral</button> 
+      
+    </>
+  )
+}
+
+export default App
